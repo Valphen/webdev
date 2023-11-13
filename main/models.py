7cf79b3353e702a4f81dd9da46025ecde2620aa1
Конончик Станислав <100080653+Valphen@users.noreply.github.com>
2023-11-13 21:33:09 +0300

    Add files via upload
    Добавил комментарии, теги, категории в main/models.py.

--- main/models.py
+++ main/models.py
@@ -5,3 +5,13 @@ class Article(models.Model):
     text = models.TextField()
     creation_datetime = models.DateTimeField(auto_now=True)
 
+class Commentary(models.Model):                                     #У комментария есть время написания, у комментария есть автора, у комментария есть текст, у комменатрия есть количество лайков
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+class Tag(models.Model):                                            #У тега есть название с припиской #
+    name = models.CharField(max_length=50)
+
+class Category(models.Model):                                       #У категории есть название
+    name = models.CharField(max_length=100)
